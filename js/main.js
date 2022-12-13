bbf6a939c300d7ae260e64ad3d5f911eda7bc0c3
weiliw2 <weiliw2@uw.edu>
2022-12-12 19:50:23 -0800

    street style

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken ="pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw";
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/dark-v10",
+  style: "mapbox://styles/mapbox/streets-v12",
   zoom: 6.5,
   center: [-122, 47],
 });
